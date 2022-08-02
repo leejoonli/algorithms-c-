e5be9f5dcf550cee647fa728fc9bcef1185830b2
leejoonli <git@github.com>
2022-08-01 17:02:56 -0700

    add quick sort

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -8,7 +8,7 @@ namespace Algorithms
 {
     internal class Sorting
     {
-        static Array Bubblesort(int[] args)
+        static int[] Bubblesort(int[] args)
         {
             // initialize swapped as true
             bool init = true;
@@ -35,7 +35,7 @@ namespace Algorithms
             }
             return args;
         }
-        static Array Insertionsort(int[] args)
+        static int[] Insertionsort(int[] args)
         {
             // iterate across data
             for(int i = 0; i < args.Length; i++)
@@ -56,6 +56,36 @@ namespace Algorithms
             }
             // return sorted array
             return args;
+        }
+        static int[] Quicksort(int[] args)
+        {
+            // base case: the array has one element or less
+            if(args.Length <= 1)
+            {
+                return args;
+            }
+            // create the pivot, and take it out of the array (pivot is the element at the end of the array)
+            // not sure if this works and currently don't know how to run this code in the terminal
+            int pivot = args[args.Length - 1];
+            args = args.Take(args.Length - 1).ToArray();
+            // create the left partition, contains every element less than the value of pivot
+            int[] left = Array.FindAll(args, i => i < pivot);
+            // create the right partition, contains every element greater than the value of pivot
+            int[] right = Array.FindAll(args, i => i > pivot);
+            // return a new array, made up of recursively calling the quick sort with the left partition, followed by the pivot, followed by the quick sort of the right partition
+            int[] final = Quicksort(left);
+            int[] final_two = Quicksort(right);
+            int[] recursive_final = new int[final.Length + final_two.Length + 1];
+            for(int i = 0; i < final.Length; i++)
+            {
+                recursive_final.Append(final[i]);
+            }
+            recursive_final.Append(pivot);
+            for(int i = 0; i < final_two.Length; i++)
+            {
+                recursive_final.Append(final_two[i]);
+            }
+            return recursive_final;
         }
     }
 }
